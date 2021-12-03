f2f930091e54730862d48eecd0e12e0d2a354200
ashish-patwal <ashishpatwal147@gmail.com>
2021-12-03 18:28:29 +0530

    theme-toggle and dark mode

--- App.tsx
+++ App.tsx
@@ -1,24 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import AppContainer from './src/components/app-container'
+import Main from './src/screens/main'
 
 export default function App() {
   return (
     <AppContainer>
-      <View style={styles.container}>
-        <Text>My name is Ashish Patwal.</Text>
-        <StatusBar style="auto" />
-      </View>
+      <Main />
     </AppContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -2,13 +2,22 @@ import * as React from 'react'
 import {
   Text,
   Box,
-  Center
+  Center,
+  VStack,
+  themeTools,
+  useTheme,
+  useColorMode,
+  useColorModeValue
 } from 'native-base'
+import ThemeToggle from '../components/theme-toggle'
 
 export default function MainScreen() {
   return (
     <Center _dark={{ bg: 'blueGray.900' }} _light={{ bg: 'blueGray.50' }} px={4} flex={1}>
-
+      <VStack space={5} alignItems="center">
+        <Box><Text>Hello Ashish</Text></Box>
+        <ThemeToggle />
+      </VStack>
     </Center>
   )
 }
